a84e99b14cbc8bbb17df18c9db9261a7e51859eb
Руслан Солдатов <ruslansoldatov@MBP-Ruslan.g-service.ru>
2024-05-14 17:06:28 +0700

    адаптация

--- script/script.js
+++ script/script.js
@@ -1,4 +1,10 @@
- $(document).ready(function() {
+const btn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+
+btn.addEventListener('click', ()=>{
+    nav.classList.toggle('menu-open');
+});
+$(document).ready(function() {
     $(window).scroll(function() {
         $(".animated").each(function() {
             var position = $(this).offset().top;
@@ -10,55 +16,3 @@
         });
     });
 });
-
-
-document.getElementById('review-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    
-    var name = document.getElementById('review-name').value;
-    var text = document.getElementById('review-text').value;
-    
-    var reviewElement = document.createElement('div');
-    reviewElement.classList.add('review');
-    
-    var authorElement = document.createElement('p');
-    authorElement.classList.add('review-author');
-    authorElement.textContent = name;
-    
-    var textElement = document.createElement('p');
-    textElement.classList.add('review-text');
-    textElement.textContent = text;
-    
-    reviewElement.appendChild(authorElement);
-    reviewElement.appendChild(textElement);
-
-    var reviewList = document.getElementById('review-list');
-    reviewList.appendChild(reviewElement);
-    
-    document.getElementById('review-name').value = '';
-    document.getElementById('review-text').value = '';
-    
-});
-
-
-    window.addEventListener("resize", AutoScale); 
-
-    AutoScale();
-    
-    function AutoScale()
-    {
-        let width = window.innerWidth; 
-    
-        if(width > 1280)
-        {
-         ChangeScale("big");
-        }
-        else if(width <= 1280 && width > 720)
-        {
-         ChangeScale("normal");
-        }
-        else if(width < 720)
-        {
-         ChangeScale("small");
-        }
-    }
